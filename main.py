baaea19ca0adec7103943ca0a5f538c8e5ee32a6
Mott Fisk <mottfisk@comcast.net>
2012-08-23 21:12:36 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Word Count
+# Program will count the number of words and the number of occurences for each word.
+# Author:  Mott Fisk
+
+def count_words(text):
+    return 1
+
+if __name__ == '__main__':
+    print count_words('i went to the store')
---
+++ test.py
@@ -0,0 +1,6 @@
+import main
+
+if main.count_words('hi') == 1:
+    print 'success'
+else:
+    print 'failed'
